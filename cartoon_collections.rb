eae1c7bc9fc0361c7d6894cda8ae7d11cb154785
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-06 00:55:51 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,7 +4,7 @@ def greet_characters(array)
   # Print a custom greeting for each element
   
   array.each do |index|
-    puts "Hello #{index} wants to start coding!"
+    puts "Hello #{index}!"
   end
 end
 
